e35b9f73c44b71e2a42894a210aa2c5b3a124871
Froggles <elmsterm@gmail.com>
2026-02-06 12:17:06 -0500

    reformat guest list items

--- src/components/GuestItem.jsx
+++ src/components/GuestItem.jsx
@@ -2,8 +2,7 @@ export default function GuestItem({ guest, selectGuestId }) {
   return (
     <li onClick={() => selectGuestId(guest.id)}>
       <dl>
-        <dt>Name</dt> <dd>{guest.name}</dd>
-        <dt>Email</dt> <dd>{guest.email}</dd>
+        <dt>{guest.name}</dt> <dd>{guest.email}</dd>
       </dl>
     </li>
   );
